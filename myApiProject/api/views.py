3c7ce09748d996ddb88cbbd741c0c8ade56d1f95
arthurbfranca <48159125+arthurbfranca@users.noreply.github.com>
2020-12-01 16:08:08 -0700

    Posttown

--- myApiProject/api/views.py
+++ myApiProject/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer
-from .models import User
+from .serializers import *
+from .models import *
 
 class UserList (APIView):
     def get(self, request, format=None):
@@ -39,3 +39,17 @@ class UserDetail (APIView):
         user = User.objects.filter (pk=pk)
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class EmployeeList (APIView):
+    def get(self, request, format=None):
+        users = Employee.objects.all()
+        serializer = EmployeeSerializer (users, many=True)
+        return Response (serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer (data=request.data)
+        if serializer.is_valid ( ):
+            serializer.save ( )
+            return Response (serializer.data, status=status.HTTP_201_CREATED)
+        return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
